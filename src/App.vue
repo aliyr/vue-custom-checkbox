f20878d9470178dea9c6023ece72f42fc048175c
Alious <aliyr7592@gmail.com>
2019-02-04 12:44:22 +0330

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <Checkbox v-model="value">
+      hello world
+    </Checkbox>
+  </div>
+</template>
+
+<script>
+
+import Checkbox from "./components/Checkbox";
+export default {
+  name: 'app',
+  components: {
+    Checkbox
+  },
+  data: function () {
+    return {
+      value: false
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
